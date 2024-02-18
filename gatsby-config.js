2b117b263322c988afa4caa958212a744b7add28
Syukrie77 <syukrie77@gmail.com>
2024-02-18 20:55:57 +0700

    update ke 6

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,13 +41,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Aretanet-Blog`,
+        name: `AretanetBlog`,
         short_name: `aretanet`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/logoaretanet.jpg`, // This path is relative to the root of the site.
       },
     },
   ],
